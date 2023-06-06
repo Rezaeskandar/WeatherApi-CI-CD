27bb02c0f6d604c0c3295b76950fe21036ab33ea
Md-Ruhul-Amin-Rony <adiar1234@yahoo.com>
2023-06-06 10:29:10 +0200

    adding DataContext service

--- WeatherApi_CI-CD/Program.cs
+++ WeatherApi_CI-CD/Program.cs
@@ -5,6 +5,7 @@
 //[]Som systemägare vill jag kunna se statistik på antal anrop sen API:et startades
 //[]Som slutanvändare av Reactklienten vill jag kunna se aktuellt väder för Stockholm
 //[]Som slutanvändare av Reactklienten vill jag kunna se och spara favoritstad
+using Microsoft.EntityFrameworkCore;
 using WeatherApi_CI_CD;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -13,6 +14,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<DataContext>(c => c.UseInMemoryDatabase("City"));
 
 var app = builder.Build();
 
