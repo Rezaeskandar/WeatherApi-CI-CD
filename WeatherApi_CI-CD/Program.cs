9d01dc51fa6dea9d020568da18300adafdc0b07f
Md-Ruhul-Amin-Rony <adiar1234@yahoo.com>
2023-06-06 10:31:19 +0200

    adding healthcheck service and cors

--- WeatherApi_CI-CD/Program.cs
+++ WeatherApi_CI-CD/Program.cs
@@ -14,7 +14,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddDbContext<DataContext>(c => c.UseInMemoryDatabase("City"));
+builder.Services.AddDbContext<DataContext>(c => c.UseInMemoryDatabase("City"));//here we have used memory database
 
 var app = builder.Build();
 
