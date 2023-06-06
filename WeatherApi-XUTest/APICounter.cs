7c9aabbdbf8118f470569c616928f46547bca13a
MD KAMRUL HASAN <114093612+chasmkhasan@users.noreply.github.com>
2023-06-06 20:23:34 +0200

    APICallCounter passed

---
+++ WeatherApi-XUTest/APICounter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherApi_XUTest
+{
+    public class APICounter
+    {
+        private int apiCallCount;
+
+        public APICounter(int initialCount)
+        {
+            apiCallCount = initialCount;
+        }
+
+        public async Task IncrementAPICallAsync()
+        {
+            await Task.Run(() => Interlocked.Increment(ref apiCallCount));
+        }
+
+        public async Task<string> GetStatisticsAsync()
+        {
+            return await Task.Run(() => $"Number of API calls: {apiCallCount}");
+        }
+
+        public int GetAPICallCount()
+        {
+            return apiCallCount;
+        }
+    }
+}
