8f362f57cb665358df4bc7dcbc5c2b9075e66075
Md-Ruhul-Amin-Rony <adiar1234@yahoo.com>
2023-06-08 09:55:17 +0200

    Test Fail for getting cities

---
+++ WeatherApi-XUTest/GetFavoriteCities.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Xunit;
+
+
+public class GetFavoriteCities
+{
+    [Fact]
+    public async Task GetFavoriteCities_ReturnsListOfCities()
+    {
+        // Arrange
+        var options = new DbContextOptionsBuilder<DataContext>()
+            .UseInMemoryDatabase(databaseName: "TestDatabase")
+            .Options;
+
+        using (var context = new DataContext(options))
+        {
+            // Prepare the test data
+            var cities = new List<City>
+        {
+            new City { Name = "Stockholm" },
+            new City { Name = "Gothenburg" },
+            new City { Name = "Malmö" }
+        };
+            context.Cities.AddRange(cities);
+            context.SaveChanges();
+
+            var controller = new GetFavoriteCitiesController(context);
+
+            // Act
+            var result = await controller.GetFavoriteCities();
+
+            // Assert 
+            Assert.Empty(result.Value); // Expecting the result to be empty initially
+        }
+    }
+}
+
+internal class GetFavoriteCitiesController
+{
+    private DataContext context;
+
+    public GetFavoriteCitiesController(DataContext context)
+    {
+        this.context = context;
+    }
+    public async Task<ActionResult<List<City>>> GetFavoriteCities()
+    {
+        var cities = await context.Cities.ToListAsync();
+        return cities;
+    }
+}
