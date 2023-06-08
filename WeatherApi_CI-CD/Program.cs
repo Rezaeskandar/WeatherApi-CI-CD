0c3fd5b7784198b93ea67f067b997995e59ba423
Reza Eskandari <rezaeskandari.ammori@yahoo.com>
2023-06-08 21:38:21 +0200

    bump

--- WeatherApi_CI-CD/Program.cs
+++ WeatherApi_CI-CD/Program.cs
@@ -1,16 +1,15 @@
 
 
+//TODOS
 
 //[x]As a user of the API, I want to be able to get current weather data (temperature, humidity, wind) for Stockholm.
+//[x]Som användare av API:et vill jag kunna spara en favoritstad och slippa ange den varje gång(Obs att det bara ska sparas så länge appen körs, alltså inte mellan körningar)
+//[x]Som systemägare vill jag kunna se om API:et körs(health check)
+//[x]Som systemägare vill jag kunna se statistik på antal anrop sen API:et startades
+//[x]Som slutanvändare av Reactklienten vill jag kunna se aktuellt väder för Stockholm
+//[x]Som slutanvändare av Reactklienten vill jag kunna se och spara favoritstad
 
 using Microsoft.EntityFrameworkCore;
-
-//[]Som användare av API:et vill jag kunna spara en favoritstad och slippa ange den varje gång(Obs att det bara ska sparas så länge appen körs, alltså inte mellan körningar)
-//[]Som systemägare vill jag kunna se om API:et körs(health check)
-//[]Som systemägare vill jag kunna se statistik på antal anrop sen API:et startades
-//[]Som slutanvändare av Reactklienten vill jag kunna se aktuellt väder för Stockholm
-//[]Som slutanvändare av Reactklienten vill jag kunna se och spara favoritstad
-
 using WeatherApi_CI_CD;
 
 var builder = WebApplication.CreateBuilder(args);
