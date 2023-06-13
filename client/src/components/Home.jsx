c15699c923cdf385751774fc0e7d1b4d9d310879
Md-Ruhul-Amin-Rony <adiar1234@yahoo.com>
2023-06-13 20:58:26 +0200

    make two components

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,6 +1,98 @@
+// import React, { useState, useEffect } from 'react';
+// import axios from 'axios';
+// import styled from 'styled-components' ;
+
+// const Background = styled.div`
+//   background: linear-gradient(135deg, #00c6ff, #0072ff);
+//   min-height: 100vh;
+//   display: flex;
+//   align-items: center;
+//   justify-content: center;
+// `;
+
+// const Container = styled.div`
+//   background-color: rgba(248, 248, 248, 0.8);
+//   padding: 40px;
+//   border-radius: 12px;
+//   text-align: center;
+//   box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
+//   backdrop-filter: blur(8px);
+// `;
+
+// const WeatherInfo = styled.div`
+//   margin-bottom: 20px;
+// `;
+
+// const Temperature = styled.div`
+//   font-size: 48px;
+//   font-weight: bold;
+//   color: hsl(207, 75%, 43%);
+// `;
+
+// const WeatherProperty = styled.div`
+//   font-size: 24px;
+//   color: hsl(0, 0%, 40%);
+// `;
+
+
+// const StockholmWeather = () => {
+//   const [weatherData, setWeatherData] = useState(null);
+
+//   useEffect(() => {
+//     const fetchData = async () => {
+//       try {
+//         const response = await axios.get('https://dev.kjeld.io:40200/weatherforecast-Stockholm');
+//         const data = response.data;
+//         setWeatherData(data.weatherData);
+//       } catch (error) {
+//         console.error(error);
+//       }
+//     };
+
+//     fetchData();
+//   }, []);
+
+//   return (
+//     <Background>
+//       <Container>
+//         {weatherData && (
+//           <>
+//             <WeatherInfo>
+//               <Temperature>{weatherData.temperature.value} {weatherData.temperature.unit}</Temperature>
+//               <WeatherProperty>{weatherData.location}</WeatherProperty>
+//             </WeatherInfo>
+//             <WeatherProperty>
+//               Humidity: {weatherData.humidity.value} {weatherData.humidity.unit}
+//             </WeatherProperty>
+//             <WeatherProperty>
+//               Wind: {weatherData.wind.value} {weatherData.wind.unit}
+//             </WeatherProperty>
+
+//           </>
+//         )}
+//       </Container>
+//     </Background>
+//   );
+// };
+
+// export default StockholmWeather;
+
+
+
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import styled from 'styled-components' ;
+import styled from 'styled-components';
+
+const Button = styled.button`
+  font-size: 16px;
+  padding: 10px 20px;
+  border-radius: 8px;
+  background-color: #fff;
+  color: #0072ff;
+  border: 1px solid #0072ff;
+  cursor: pointer;
+  outline: none;
+`;
 
 const Background = styled.div`
   background: linear-gradient(135deg, #00c6ff, #0072ff);
@@ -20,42 +112,45 @@ const Container = styled.div`
 `;
 
 const WeatherInfo = styled.div`
-  margin-bottom: 20px;
-`;
+  margin-bottom: 20px`; 
 
-const Temperature = styled.div`
-  font-size: 48px;
-  font-weight: bold;
+ const Temperature = styled.div`
+   font-size: 48px;
+   font-weight: bold;
   color: hsl(207, 75%, 43%);
 `;
 
-const WeatherProperty = styled.div`
-  font-size: 24px;
-  color: hsl(0, 0%, 40%);
-`;
+ const WeatherProperty = styled.div`
+   font-size: 24px;
+   color: hsl(0, 0%, 40%);
+ `;
+
+
 
 
 const StockholmWeather = () => {
   const [weatherData, setWeatherData] = useState(null);
+  const [showWeather, setShowWeather] = useState(false);
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await axios.get('https://dev.kjeld.io:40200/weatherforecast-Stockholm');
-        const data = response.data;
-        setWeatherData(data.weatherData);
-      } catch (error) {
-        console.error(error);
-      }
-    };
+  const fetchData = async () => {
+    try {
+      const response = await axios.get('https://dev.kjeld.io:40200/weatherforecast-Stockholm');
+      const data = response.data;
+      setWeatherData(data.weatherData);
+      setShowWeather(true);
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
-    fetchData();
-  }, []);
+  const handleClose = () => {
+    setShowWeather(false);
+  };
 
   return (
     <Background>
       <Container>
-        {weatherData && (
+        {showWeather ? (
           <>
             <WeatherInfo>
               <Temperature>{weatherData.temperature.value} {weatherData.temperature.unit}</Temperature>
@@ -67,8 +162,10 @@ const StockholmWeather = () => {
             <WeatherProperty>
               Wind: {weatherData.wind.value} {weatherData.wind.unit}
             </WeatherProperty>
-
+            <Button onClick={handleClose}>Close</Button>
           </>
+        ) : (
+          <Button onClick={fetchData}>Get Stockholm Weather</Button>
         )}
       </Container>
     </Background>
@@ -76,3 +173,6 @@ const StockholmWeather = () => {
 };
 
 export default StockholmWeather;
+
+
+
--- client/src/components/Statistices.jsx
+++ client/src/components/Statistices.jsx
@@ -1,7 +1,65 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import styled from 'styled-components' ;
+// import React, { useState, useEffect } from 'react';
+// import axios from 'axios';
+// import styled from 'styled-components' ;
 
+
+// const Background = styled.div`
+//   background: linear-gradient(135deg, #33ff00, #00ff9d);
+//   min-height: 100vh;
+//   display: flex;
+//   align-items: center;
+//   justify-content: center;
+// `;
+
+// const Container = styled.div`
+//   background-color: rgba(248, 248, 248, 0.8);
+//   padding: 40px;
+//   border-radius: 12px;
+//   text-align: center;
+//   box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
+//   backdrop-filter: blur(8px);
+// `;
+
+// const Statistics = styled.div`
+//   margin-bottom: 20px;
+// `;
+
+// function Statistices() {
+//   const [apiCall, setApiCall] = useState([]);
+ 
+//    useEffect(() => {
+//     const fetchData = async () => {
+//       try {
+//         const response = await axios.get('https://dev.kjeld.io:40200/API/call/statistics');
+//         const data = response.data;
+//         setApiCall(data.apiCall);
+//       } catch (error) {
+//         console.error(error);
+//       }
+//     };
+
+//     fetchData();
+//   }, []);
+
+//   return(
+//     <>
+//     <Background>
+//         <Container>
+//             <Statistics>We have Called API {apiCall} </Statistics>
+//         </Container>
+//     </Background>
+       
+//     </>
+        
+//   )
+// }
+
+// export default Statistices;
+
+
+import React, { useState } from 'react';
+import axios from 'axios';
+import styled from 'styled-components';
 
 const Background = styled.div`
   background: linear-gradient(135deg, #33ff00, #00ff9d);
@@ -24,35 +82,37 @@ const Statistics = styled.div`
   margin-bottom: 20px;
 `;
 
-function Statistices() {
-  const [apiCall, setApiCall] = useState([]);
- 
-   useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await axios.get('https://dev.kjeld.io:40200/API/call/statistics');
-        const data = response.data;
-        setApiCall(data.apiCall);
-      } catch (error) {
-        console.error(error);
-      }
-    };
+function StatisticsComponent() {
+  const [apiCall, setApiCall] = useState(0);
 
-    fetchData();
-  }, []);
+  const fetchData = async () => {
+    try {
+      const response = await axios.get('https://dev.kjeld.io:40200/API/call/statistics');
+      const data = response.data;
+      // Extract the count from the response string
+      //const count = parseInt(data.match(/\d+/)[0]);
+      //const count = parseInt(data, 10) || 0;//parseInt(data, 10) - The parseInt function is used to parse the data string as an integer. The second argument 10 specifies the base (decimal) for parsing.
+      const count = parseInt(data.split(':')[1].trim()) || 0;//splits the response string by ":" and retrieves the second part, which represents the count value. The trim() function is used to remove any leading or trailing 
+      setApiCall(count);
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
-  return(
-    <>
+  const handleButtonClick = async () => {
+    await fetchData();
+  };
+
+  return (
     <Background>
-        <Container>
-            <Statistics>We have Called API {apiCall} </Statistics>
-        </Container>
+      <Container>
+        <Statistics>We have Made {apiCall} API Calls</Statistics>
+        <button onClick={handleButtonClick}>Get API Calls</button>
+      </Container>
     </Background>
-       
-    </>
-        
-  )
+  );
 }
 
-export default Statistices;
+export default StatisticsComponent;
+
 
